83a73020670612f2bb6f7d67a89451d5c083f3e6
Alen Miah <71030205+AlenMiah@users.noreply.github.com>
2022-02-05 13:28:20 -0500

    Update application_helper.rb
    changed the asc and desc around so that the first click of the header sets it to reverse sort on team name.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,7 +14,7 @@
 module ApplicationHelper
   def sorting_teams(column, title = nil)
     title ||= column.titleize
-    direction = (column == params[:sort] && params[:direction] == "asc") ? "desc" : "asc"
+    direction = (column == params[:sort] && params[:direction] == "desc") ? "asc" : "desc"
     link_to title, :sort => column, :direction => direction
   end
 end
