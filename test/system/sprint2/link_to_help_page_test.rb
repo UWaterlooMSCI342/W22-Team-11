20bd9a9c6330fbb197b57d6b71844493d6dd176e
Jovina Juana <jjuana@uwaterloo.ca>
2022-02-28 10:43:49 -0500

    fixing changes

--- test/system/sprint2/link_to_help_page_test.rb
+++ test/system/sprint2/link_to_help_page_test.rb
@@ -10,21 +10,23 @@ class LinkToHelpPage < ApplicationSystemTestCase
 
     def test_link_to_help_page_from_home
         visit root_url
-        login 'zappy@gmail.com', 'banana'
+        login 'zappy@gmail.com','banana'
+        assert_current_path root_url
 
-        click_on 'Help'
+        click_link 'Help'
         assert_text 'Help page'
     end
 
     def test_link_to_help_page_from_submit_feedback
         visit root_url
-        login 'zappy@gmail.com', 'banana'
+        login 'zappy@gmail.com','banana'
+        assert_current_path root_url
 
-        click_on 'Submit for: Test Team'
+        click_link 'Submit for: Test Team'
         assert_current_path new_feedback_url
         assert_text 'Your Current Team: Test Team'
 
-        click_on 'Help'
+        click_link 'Help'
         assert_text 'Help Page'
     end
 
