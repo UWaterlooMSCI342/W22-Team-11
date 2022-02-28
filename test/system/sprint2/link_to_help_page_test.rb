ef7c21b88281b7907ed914f4bca361aa121f3fd6
Jovina Juana <jjuana@uwaterloo.ca>
2022-02-28 11:19:33 -0500

    fixed automated testing

--- test/system/sprint2/link_to_help_page_test.rb
+++ test/system/sprint2/link_to_help_page_test.rb
@@ -1,25 +1,26 @@
 require "application_system_test_case"
 
 class LinkToHelpPage < ApplicationSystemTestCase
-    def setup
+    setup do
+        @bob = User.create(email: 'bob@gmail.com', first_name: 'Bob', last_name: 'Smith', is_admin: false, password: 'testpassword', password_confirmation: 'testpassword')
         @prof = User.create(email: 'msmucker@gmail.com', first_name: 'Mark', last_name: 'Smucker', is_admin: true, password: 'professor', password_confirmation: 'professor')
-        @team = Team.create(team_name: 'Test Team', team_code: 'TEAM01', user: @prof)
-        @user = User.create(email: 'zappy@gmail.com', password: 'banana', password_confirmation: 'banana', first_name: 'Zappy', last_name: 'Zoo', is_admin: false, teams: [@team])
-        @user.save
+        @generated_code = Team.generate_team_code
+        @team = Team.create(team_name: 'Test Team', team_code: 'TEAM00', user: @prof, capacity: 1)
+        @bob.teams << @team
     end
 
     def test_link_to_help_page_from_home
         visit root_url
-        login 'zappy@gmail.com','banana'
+        login 'bob@gmail.com','testpassword'
         assert_current_path root_url
 
         click_link 'Help'
-        assert_text 'Help page'
+        assert_text 'Help Page'
     end
 
     def test_link_to_help_page_from_submit_feedback
         visit root_url
-        login 'zappy@gmail.com','banana'
+        login 'bob@gmail.com','testpassword'
         assert_current_path root_url
 
         click_link 'Submit for: Test Team'
