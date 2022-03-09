49e3ced7a896ea18f7575b44519054bc43650f83
Nicole D'Souza <n6dsouza@uwaterloo.ca>
2022-03-09 16:09:07 -0500

    Creating link to user profile from manage users

---
+++ test/system/sprint3/user_profile_link_test.rb
@@ -0,0 +1,25 @@
+require "application_system_test_case"
+
+class LinksToUserProfileTest < ApplicationSystemTestCase
+   def setup 
+    #@prof = User.create(email: 'msmucker@gmail.com', name: 'Mark Smucker', is_admin: true, password: 'professor', password_confirmation: 'professor')
+    @prof = User.create(email: 'msmucker@gmail.com', first_name: 'Mark', last_name: 'Smucker', is_admin: true, password: 'professor', password_confirmation: 'professor')
+    @team = Team.create(team_name: 'Test Team', team_code: 'TEAM01', user: @prof)
+    #@user = User.create(email: 'zappy@gmail.com', password: 'banana', password_confirmation: 'banana', name: 'Zappy Zoo', is_admin: false, teams: [@team])
+    @user = User.create(email: 'zappy@gmail.com', password: 'banana', password_confirmation: 'banana', first_name: 'Zappy', last_name: 'Zoo', is_admin: false, teams: [@team])
+    @user.save
+    end
+
+    def test_link_to_user_profile_from_manage_users
+        visit root_url
+        login 'msmucker@gmail.com','professor'
+        assert_current_path root_url
+        visit users_url
+        assert_current_path users_url
+        #click_on "Zappy"
+        #click_link(@user)
+        #assert_current_path user_path(@user)
+    end
+
+end
+
