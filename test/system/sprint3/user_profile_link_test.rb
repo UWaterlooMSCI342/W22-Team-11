b5439b2beed6cc0464a06b68f8d8598b36b1aa91
Nicole D'Souza <n6dsouza@uwaterloo.ca>
2022-03-09 16:27:37 -0500

    Link to user profile from manage users

--- test/system/sprint3/user_profile_link_test.rb
+++ test/system/sprint3/user_profile_link_test.rb
@@ -2,12 +2,12 @@ require "application_system_test_case"
 
 class LinksToUserProfileTest < ApplicationSystemTestCase
    def setup 
-    #@prof = User.create(email: 'msmucker@gmail.com', name: 'Mark Smucker', is_admin: true, password: 'professor', password_confirmation: 'professor')
+    @bob = User.create(email: 'bob@gmail.com', first_name: 'Bob', last_name: 'Smith', is_admin: false, password: 'testpassword', password_confirmation: 'testpassword')
     @prof = User.create(email: 'msmucker@gmail.com', first_name: 'Mark', last_name: 'Smucker', is_admin: true, password: 'professor', password_confirmation: 'professor')
-    @team = Team.create(team_name: 'Test Team', team_code: 'TEAM01', user: @prof)
-    #@user = User.create(email: 'zappy@gmail.com', password: 'banana', password_confirmation: 'banana', name: 'Zappy Zoo', is_admin: false, teams: [@team])
-    @user = User.create(email: 'zappy@gmail.com', password: 'banana', password_confirmation: 'banana', first_name: 'Zappy', last_name: 'Zoo', is_admin: false, teams: [@team])
-    @user.save
+    @generated_code = Team.generate_team_code
+    @team = Team.create(team_name: 'Test Team', team_code: 'TEAM00', user: @prof, capacity: 1)     
+    @bob.teams << @team
+
     end
 
     def test_link_to_user_profile_from_manage_users
@@ -16,8 +16,8 @@ class LinksToUserProfileTest < ApplicationSystemTestCase
         assert_current_path root_url
         visit users_url
         assert_current_path users_url
-        #click_on "Zappy"
-        #click_link(@user)
+        click_on "Bob"
+        #click_link(@bob)
         #assert_current_path user_path(@user)
     end
 
