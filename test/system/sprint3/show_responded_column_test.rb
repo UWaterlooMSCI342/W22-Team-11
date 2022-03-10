3d1ed7be4ba4f5964998c84c1239aa2f8e72e24a
Anday Duman <a2duman@uwaterloo.ca>
2022-03-10 00:16:17 -0500

    Created automated tests for ensuring that responded column only has a checkbox and save button if the rating is <= 6

--- test/system/sprint3/show_responded_column_test.rb
+++ test/system/sprint3/show_responded_column_test.rb
@@ -5,8 +5,13 @@ class ShowRespondedColumnTest < ApplicationSystemTestCase
     @bob = User.create(email: 'bob@gmail.com', first_name: 'Bob', last_name: 'Smith', is_admin: false, password: 'testpassword', password_confirmation: 'testpassword')
     @prof = User.create(email: 'msmucker@gmail.com', first_name: 'Mark', last_name: 'Smucker', is_admin: true, password: 'professor', password_confirmation: 'professor')
     @generated_code = Team.generate_team_code
-    @team = Team.create(team_name: 'Test Team', team_code: 'TEAM00', user: @prof, capacity: 1)     
+    @team = Team.create(team_name: 'Test Team', team_code: 'TEAM00', user: @prof, capacity: 2)     
     @bob.teams << @team
+    @feedback = Feedback.new(rating: 1, communication:1, collaboration:1, team_support:1, responsibility:1, work_quality:1, comments: "No comment", priority: 0)
+    datetime = Time.current
+    @feedback.timestamp = @feedback.format_time(datetime)
+    @feedback.team = @bob.teams.first
+    @bob.feedbacks << @feedback
 
     end
 
@@ -14,11 +19,24 @@ class ShowRespondedColumnTest < ApplicationSystemTestCase
         visit root_url
         login 'msmucker@gmail.com','professor'
         assert_current_path root_url
-        visit users_url
-        assert_current_path users_url
-        click_on "Bob"
-        #click_link(@bob)
-        assert_current_path user_path(@bob)
+        click_on "Feedback & Ratings"
+        assert_current_path feedbacks_path
+        click_button "Save"
+    end
+
+    def test_no_show_responded_column_if_high_feedback
+        @feedback.rating = 10
+        @feedback.save
+
+        visit root_url
+        login 'msmucker@gmail.com','professor'
+        assert_current_path root_url
+        click_on "Feedback & Ratings"
+        assert_current_path feedbacks_path
+        assert_text "10.0"
+        #Received inspiration from this post for using has_no_button?: https://stackoverflow.com/questions/5906039/ensure-an-element-is-not-present-with-capybara
+        assert has_no_button?('Save')
+        
     end
 
 end
